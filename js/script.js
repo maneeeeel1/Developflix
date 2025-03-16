2d9373217eb8b494176195bc89d75468b7248ec4
Manel Bueno <bmanelb@gmail.com>
2025-03-16 17:48:14 +0100

    pagina cine

--- js/script.js
+++ js/script.js
@@ -1,2 +1,25 @@
 import peliculas from './peliculas.js'
+const URL_BASE_IMAGE ="https://image.tmdb.org/t/p/w500";
 
+
+
+function cargarPelis(){
+
+    peliculas.forEach(pelicula =>{
+        pelicula.genre_ids.forEach(genero =>{
+            const contenedor = document.getElementById(`genero-${genero}`);
+            if (contenedor){
+                const peligenero = document.createElement("div");
+                peligenero.classList.add("pelicula");
+                peligenero.innerHTML=`<img src="${URL_BASE_IMAGE}${pelicula.poster_path}">
+                <p>${pelicula.original_title}</p>`;
+
+                contenedor.appendChild(peligenero);
+            }
+
+        });
+    });
+}
+
+
+document.addEventListener("DOMContentLoaded", cargarPelis);
